9305a28efd852fcaeb564f7fbdcba1a71cebe6fa
Dmitrii Kniazev <dmitriikniazev@gmail.com>
2022-11-17 23:59:10 +0400

    rewritten from scratch

--- example_5.6/Program.cs
+++ example_5.6/Program.cs
@@ -4,119 +4,73 @@ namespace example_5._6
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static (string FirstName, string LastName, int Age, bool HasPets, int PetsNumber, string[] PetsNames, int FavColorsNumber, string[] FavColors) UserData()
         {
-            string FirstName = "empty";
-            string LastName = "empty";
-            int Age = 0;
-            bool HasPets = false;
-            string[] PetsNames = Array.Empty<string>();
-            int favColorsNumber = 0;
-            string[] FavColors = Array.Empty<string>();
+            (string FirstName, string LastName, int Age, bool HasPets, int PetsNumber, string[] PetsNames, int FavColorsNumber, string[] FavColors) User;
 
-
-            UserProfile(FirstName, LastName, Age, HasPets, PetsNames, favColorsNumber, FavColors);
-        }
-
-        static object UserProfile(string FirstName, string LastName, int Age, bool HasPets, string[] PetsNames, int favColorsNumber, string[] FavColors)
-        {
-            (string FirstName, string LastName, int Age, bool HasPets, string[] PetsNames, int favColorsNumber, string[] FavColors) User;
-            
+            //ask first name
             Console.WriteLine("Enter your name: ");
             User.FirstName = Console.ReadLine();
 
+            //ask last name
             Console.WriteLine("Enter your last name: ");
             User.LastName = Console.ReadLine();
 
+            //ask age
             Console.WriteLine("How old are you?");
-            bool isNumeric = int.TryParse(Console.ReadLine(), out User.Age); 
+            bool isNumeric = int.TryParse(Console.ReadLine(), out User.Age);
             if (!isNumeric || User.Age < 0) //check if the entered value is numeric and greater than 0
             {
                 User.Age = NumberCorrection();
             }
 
+            //ask if a user has pets
             Console.WriteLine("Do you have a pet? (yes/no)");
-            bool UserHasPets = Console.ReadLine().Equals("yes", StringComparison.CurrentCultureIgnoreCase); //accepting yes without case (YES, Yes, yes, etc.)
-
-            //initialize the arrays, not sure how else to do it??
-            User.PetsNames = Array.Empty<string>();
-            User.FavColors = Array.Empty<string>(); ;
+            User.HasPets = Console.ReadLine().Equals("yes", StringComparison.CurrentCultureIgnoreCase); //accepting yes without case (YES, Yes, yes, etc.)
 
             //ask how many pets a user have
-            if (UserHasPets == true)
+            User.PetsNumber = 0; //need to initialize PetsNumber, otherwise "return User" might throw 
+            if (User.HasPets == true)
             {
                 Console.WriteLine("How many pets do you have?");
-                bool numericCheck = int.TryParse(Console.ReadLine(), out int petsNum);
-                if (!numericCheck || petsNum < 0) //check if the entered value is numeric and greater than 0
+                bool numericCheck = int.TryParse(Console.ReadLine(), out User.PetsNumber);
+                if (!numericCheck || User.PetsNumber < 0) //check if the entered value is numeric and greater than 0
                 {
-                    petsNum = NumberCorrection();
+                    User.PetsNumber = NumberCorrection();
                 }
-                User.PetsNames = PetsProfile(petsNum);
+                User.PetsNames = new string[User.PetsNumber];
+                for (int i = 0; i < User.PetsNumber; i++) //get pets names into array
+                {
+                    Console.WriteLine($"Enter the name of your pet number {i + 1}");
+                    User.PetsNames[i] = Console.ReadLine();
+                }
+
+            }
+            else
+            {
+                User.PetsNames = new string[0];
             }
 
             //ask a user about the number of fav colors
-            Console.WriteLine("How many favorite colors do you have?"); 
-            bool checkNumeric = int.TryParse(Console.ReadLine(), out int favColorsNum);
-            if (!checkNumeric || favColorsNum < 0) //check if the entered value is numeric and greater than 0
+            Console.WriteLine("How many favorite colors do you have?");
+            bool checkNumeric = int.TryParse(Console.ReadLine(), out User.FavColorsNumber);
+            if (!checkNumeric || User.FavColorsNumber < 0) //check if the entered value is numeric and greater than 0
             {
-                favColorsNum = NumberCorrection();
+                User.FavColorsNumber = NumberCorrection();
             }
-            
 
             //create an array of all fav colors
-            User.FavColors = new string[favColorsNum];
-            for (int i = 0; i < favColorsNum; i++)
+            User.FavColors = new string[User.FavColorsNumber];
+            for (int i = 0; i < User.FavColorsNumber; i++)
             {
                 Console.WriteLine($"Enter your favorite color number {i + 1}:");
                 User.FavColors[i] = Console.ReadLine();
             }
 
-
-            //print out the info about the user
-            Console.WriteLine("=============================");
-            Console.WriteLine($"Hi, {User.FirstName} {User.LastName}, you're {User.Age} years old");
-            
-            //print pets names if the user has them
-            if(UserHasPets == true)
-            {
-                Console.WriteLine("You have the following pets:");
-                foreach (string petName in User.PetsNames)
-                {
-                    Console.WriteLine(petName);
-                }
-            }
-            else
-            {
-                Console.WriteLine("You don't have any pets");
-            }
-
-            //print out the colors info
-            Console.WriteLine($"You have {favColorsNum} favorite colors. They are: ");
-            foreach(string favColor in User.FavColors)
-            {
-                Console.WriteLine(favColor);
-            }
-
-            return (FirstName, LastName, Age, HasPets, PetsNames, favColorsNum, FavColors);
-
-
+            return User;
         }
 
-        //a method to list all pets names
-        static string[] PetsProfile(int numberOfPets)
-        {
-            string[] petsNames = new string[numberOfPets];
 
-            for (int i = 0; i < numberOfPets; i++)
-            {
-                Console.WriteLine($"Enter the name of your pet number {i + 1}");
-                petsNames[i] = Console.ReadLine();
-            }
-
-            return petsNames;
-        }
-
-        //a method that asks a user to re-enter the value until it's numeric
         static int NumberCorrection()
         {
             Console.WriteLine("You have not enetered a valid number. Please try again");
@@ -128,5 +82,48 @@ namespace example_5._6
 
             return number;
         }
+
+
+        static void Main(string[] args)
+        {
+            var (FirstName, LastName, Age, HasPets, PetsNumber, PetsNames, FavColorsNumber, FavColors) = UserData();
+
+            Console.WriteLine("=============================");
+            Console.WriteLine($"Hi, {FirstName} {LastName}, you're {Age} years old");
+
+            //print pets names if the user has them
+            if (HasPets == true)
+            {
+                Console.WriteLine($"You have {PetsNumber} pets:");
+                foreach (string petName in PetsNames)
+                {
+                    Console.WriteLine(petName);
+                }
+            }
+            else
+            {
+                Console.WriteLine("You don't have any pets");
+            }
+
+            //print out the colors info
+            if (FavColorsNumber == 0)
+            {
+                Console.WriteLine("You don't have any favorite colors");
+            }
+            else
+            {
+                Console.WriteLine($"You have {FavColorsNumber} favorite colors. They are: ");
+                foreach (string favColor in FavColors)
+                {
+                    Console.WriteLine(favColor);
+                }
+            }
+
+            Console.ReadLine();
+        }
+
     }
 }
+
+
+
